3820f60ac9cfb3f60c72cd45f8c230cdf2439d50
laynejenkins <layne.jenkins@outlook.com>
2023-02-24 15:33:50 -0800

    added comments for clarity

--- hirst_painting.py
+++ hirst_painting.py
@@ -1,3 +1,5 @@
+# used colorgram to extract the most used colors in spot_image. Also removed white-space rgb values.
+
 # import colorgram
 
 # colors = colorgram.extract("spot_image.jpg", 30)
@@ -16,15 +18,19 @@ import random
 turtle.colormode(255)
 timmy = turtle.Turtle()
 timmy.speed("fastest")
+
+# turn timmy invisible and only draw the dots.
 timmy.penup()
 timmy.hideturtle()
 
 usable_color_list = [(1, 13, 31), (52, 25, 17), (219, 127, 106), (9, 105, 160), (242, 214, 69), (150, 84, 39), (215, 87, 64), (164, 162, 32), (158, 6, 24), (157, 62, 102), (11, 63, 32),
                      (97, 6, 19), (207, 74, 104), (10, 97, 58), (0, 63, 145), (173, 135, 162), (7, 172, 216), (158, 34, 24), (3, 213, 207), (8, 140, 85), (145, 227, 216), (122, 193, 148), (102, 220, 229), (221, 178, 216), (253, 197, 0), (80, 135, 179)]
 
+# timmy's starting position cannot be in the center, so timmy needs to be moved to the bottom-right corner.
 timmy.setheading(225)
 timmy.forward(300)
 timmy.setheading(0)
+
 number_of_dots = 100
 
 for dots in range(1, number_of_dots + 1):
